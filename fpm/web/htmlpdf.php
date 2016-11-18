32acd7c1c62719ffd11fe603b9b99c75fb0207f4
Kenan Rhoton <krhoton@redsauce.net>
2016-11-18 12:19:42 +0100

    Version 0.1 Alpha! It werks!

---
+++ fpm/web/htmlpdf.php
@@ -0,0 +1,24 @@
+<?php
+
+if ($_GET['url']) {
+    $random = 3;
+
+    $file_url = "/var/www/html/pdfs/output$random.pdf";
+
+    $command = "wkhtmltopdf " . $_GET['url'] . " $file_url";
+    exec($command);
+
+    //if (file_exists($file_url)) {
+        //File Download magic
+        header('Content-Type: application/octet-stream');
+        header("Content-Transfer-Encoding: Binary");
+        header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\"");
+        readfile($file_url);
+        unlink($file_url);
+    //} else {
+        //echo "hubo un error! :(";
+    //}
+
+}
+
+?>
